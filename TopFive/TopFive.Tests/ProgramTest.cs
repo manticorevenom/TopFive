243a12f604266dd098baff8b632e39b85c46db48
manticorevenom <manticorevenom@users.noreply.github.com>
2024-02-29 12:47:22 -0500

    Adding some source files and test project

---
+++ TopFive/TopFive.Tests/ProgramTest.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using TopFive;
+using Xunit;
+
+namespace TopFive.Tests;
+
+[TestSubject(typeof(Program))]
+public class ProgramTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
---
+++ TopFive/TopFive/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TopFive;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
